e71693a65256b40ceaa2748fd7779e65bf0b1c20
Tony Li <atom2ueki@gmail.com>
2015-08-17 22:25:27 +0800

    added multiple resolution return

--- www/imagepicker.js
+++ www/imagepicker.js
@@ -1,7 +1,7 @@
 /*global cordova,window,console*/
 /**
  * An Image Picker plugin for Cordova
- * 
+ *
  * Developed by Wymsee for Sync OnSet
  */
 
@@ -13,7 +13,7 @@ var ImagePicker = function() {
 *	success - success callback
 *	fail - error callback
 *	options
-*		.maximumImagesCount - max images to be selected, defaults to 15. If this is set to 1, 
+*		.maximumImagesCount - max images to be selected, defaults to 15. If this is set to 1,
 *		                      upon selection of a single image, the plugin will return it.
 *		.width - width to resize image to (if one of height/width is 0, will resize to fit the
 *		         other while keeping aspect ratio, if both height and width are 0, the full size
@@ -26,14 +26,19 @@ ImagePicker.prototype.getPictures = function(success, fail, options) {
 		options = {};
 	}
 	
-	var params = {
-		maximumImagesCount: options.maximumImagesCount ? options.maximumImagesCount : 15,
-		width: options.width ? options.width : 0,
-		height: options.height ? options.height : 0,
-		quality: options.quality ? options.quality : 100
-	};
+	var result =[];
 
-	return cordova.exec(success, fail, "ImagePicker", "getPictures", [params]);
+	options.forEach(function(option) {
+		var params = {
+			maximumImagesCount: options.maximumImagesCount ? options.maximumImagesCount : 15,
+			width: option.width ? option.width : 0,
+			height: option.height ? option.height : 0,
+			quality: option.quality ? option.quality : 100
+		};
+		result.push(cordova.exec(success, fail, "ImagePicker", "getPictures", [params]));
+	});
+
+	return result;
 };
 
 window.imagePicker = new ImagePicker();
